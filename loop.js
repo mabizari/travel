bc129de50bd3faddc1ed1ab0702db28ec1d8d94b
mabizari abdallah <mabizariabdallah@pop-os.localdomain>
2023-06-30 00:20:00 +0100

    tp_projet_D_web

---
+++ loop.js
@@ -0,0 +1,25 @@
+ document.querySelector("a").color="green"; 
+ 
+ /*let myAdmins=["Ahmed","Osama","Sayed","Stop","Samera"];
+ let myEmployes=["Amgad","Samah","Ameer","Omar","Othman","Amany","Samia"];
+
+
+
+
+ document.write(`<div> we have ${myAdmins.indexof("stop")}</div>`);
+ document.write(`</hz>`)
+ for(let i=0;i<myAdmins.indexof("stop");i++){
+ document.write(`<h3>the admin for team ${i+1}is ${myAdmins[i]}</h3>`);
+ document.write(`<h2>Team Members :</h2>`);
+ for(let j=0;j<myEmployes.indexof("stop") ||j<myEmployes.length;j++){
+    let nmbrelement=1;
+    if(myEmployes[j].charAt(0)==myAdmins[i].charAt(0)){
+        document.write(`<p>- ${nmbrelement ," ",myEmployes[j]} </p>`);
+        nmbrelement++;
+    }
+
+ }
+}*/
+
+ 
+
